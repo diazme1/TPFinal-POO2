0f459bae32604233bddc96c64e3f7161b6058cb8
Rodriguez Perez, Diego Jesus <oxigedo@gmail.com>
2024-11-13 20:45:57 -0300

    limpiar archivos

--- tpFinal_POO2/src/tpFinal_POO2/usuario/Reserva.java
+++
@@ -1,8 +0,0 @@
-package tpFinal_POO2.usuario;
-
-import java.util.Date;
-
-public interface Reserva {
-	public Date fechaCheckIn();
-	public String getCiudad();
-}
--- tpFinal_POO2/src/tpFinal_POO2/usuario/Valoracion.java
+++
@@ -1,7 +0,0 @@
-package tpFinal_POO2.usuario;
-
-public interface Valoracion {
-	public int getVal();
-	public String getComentario();
-	public String getCategoria();
-}
