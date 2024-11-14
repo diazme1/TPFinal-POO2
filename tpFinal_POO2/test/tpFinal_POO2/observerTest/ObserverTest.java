922f6335be5a06ccb87485e225e7b05cdc0a6c0d
Emilia Diaz <diaz.m.emilia1@gmail.com>
2024-11-14 17:00:34 -0300

    Entrega finalizada con UML e informe

--- tpFinal_POO2/test/tpFinal_POO2/observerTest/ObserverTest.java
+++ tpFinal_POO2/test/tpFinal_POO2/observerTest/ObserverTest.java
@@ -25,47 +25,91 @@ class ObserverTest {
 	
 	@Test
 	void ReservasTest() {
+		//Mocks de listeners reserva:
 		ListenerReserva lReserva = mock(ListenerReserva.class);
 		ListenerReserva lReserva2 = mock(ListenerReserva.class);
+		
+		//Attach de listeners reservas al mapReservas:
 		this.observer.attachReserva(obs, lReserva);
 		this.observer.attachReserva(obs, lReserva2);
+		
+		//Notify de reserva generada:
 		this.observer.notifyReserva(obs);
+		
+		//Verify de notificaciones:
 		verify(lReserva).notifyReserva(obs);
 		verify(lReserva2).notifyReserva(obs);
+		
+		//Remove de un listener de reserva:
 		this.observer.removeReserva(obs, lReserva);
+		//Notify de reserva generada:
 		this.observer.notifyReserva(obs);
+		
+		//Verify de notifys a listeners:
 		verify(lReserva,times(1)).notifyReserva(obs);
 		verify(lReserva2,times(2)).notifyReserva(obs);
 		}
+	
+	
 	@Test
 	void BajaPrecioTest() {
+		//Mock de inmueble y listeners baja precio:
 		Inmueble obs = mock(Inmueble.class);
 		ListenerBajaPrecio lBajaPrecio = mock(ListenerBajaPrecio.class);
 		ListenerBajaPrecio lBajaPrecio2 = mock(ListenerBajaPrecio.class);
+		
+		//Attach de listener de baja de precio:
 		this.observer.attachBajaPrecio(obs, lBajaPrecio);
 		this.observer.attachBajaPrecio(obs, lBajaPrecio2);
+		
+		//Notify de baja de precio:
 		this.observer.notifyBajaPrecio(obs);
+		
+		//Verify de mensaje publish listener baja precio:
 		verify(lBajaPrecio).publish(obs);
 		verify(lBajaPrecio2).publish(obs);
+		
+		//Remove listener baja precio:
 		this.observer.removeBajaPrecio(obs, lBajaPrecio);
+		
+		//Notify baja precio:
 		this.observer.notifyBajaPrecio(obs);
+		
+		//Verify interacciones:
 		verify(lBajaPrecio,times(1)).publish(obs);
 		verify(lBajaPrecio2,times(2)).publish(obs);
+		
 		};
+		
+		
 	@Test
 	void CancelacionTest() {
+		//Mock de inmueble y listener cancelacion:
 		Inmueble obs = mock(Inmueble.class);
 		ListenerCancelacion lCancelacion = mock(ListenerCancelacion.class);
 		ListenerCancelacion lCancelacion2 = mock(ListenerCancelacion.class);
+		
+		//Attach listeners cancelacion:
 		this.observer.attachCancelacion(obs, lCancelacion);
 		this.observer.attachCancelacion(obs, lCancelacion2);
+		
+		//Noitfy cancelacion:
 		this.observer.notifyCancelacion(obs);
+		
+		//Verify de mensaje popUp listener cancelacion:
 		verify(lCancelacion).popUp(obs);
 		verify(lCancelacion2).popUp(obs);
+		
+		//Remove listener cancelacion:
 		this.observer.removeCancelacion(obs, lCancelacion);
+		
+		//Notify cancelacion:
 		this.observer.notifyCancelacion(obs);
+		
+		//Verify interacciones con listeners:
 		verify(lCancelacion,times(1)).popUp(obs);
 		verify(lCancelacion2,times(2)).popUp(obs);
+		
 		}
 
 }
